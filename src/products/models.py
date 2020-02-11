586d1627a5c834cbb55de9f802bb16d7589b0f75
jp-sypniewski <jpsypniewski@icloud.com>
2020-02-10 19:47:08 -0700

    test model types and inputs

--- src/products/models.py
+++ src/products/models.py
@@ -6,4 +6,5 @@ class Product(models.Model):
 	title 			= models.CharField(max_length=120) # max_length is required
 	description 	= models.TextField(blank=True, null=True)
 	price 			= models.DecimalField(max_digits=10, decimal_places=2)
-	summary 		= models.TextField()
+	summary 		= models.TextField(blank=True, null=False)
+	featured		= models.BooleanField()

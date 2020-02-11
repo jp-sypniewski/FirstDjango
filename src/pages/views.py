c39847014eca0de9cd441fb7b43df2fc76c3e1ac
jp-sypniewski <jpsypniewski@icloud.com>
2020-02-11 12:35:45 -0700

    add url info for about

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,9 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs): #*args, **kwargs are python specific, look these up
-	# return HttpResponse("<h1>Hello World</h1>")
-	return render(request, "home.html", {}) # return request, template name, context (empty dict for now)
 	
+	return render(request, "home.html", {}) # return request, template name, context (empty dict for now)
 
 def contact_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Contact Page</h1>")
+	return render(request, "contact.html", {})	
+
+def about_view(request, *args, **kwargs):
+	my_context = {
+		"my_text": "This is about us",
+		"my_number": 123
+	}
+	return render(request, "about.html", my_context)	
+
+def social_view(request, *args, **kwargs):
+	return HttpResponse("<h1>Social Page</h1>")

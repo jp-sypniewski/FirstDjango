8e07ed865caff1dfc0fbb302a8bb6aec54c7ea2a
jp-sypniewski <jpsypniewski@icloud.com>
2020-02-13 13:03:11 -0700

    adding object passthrough from view to template

--- src/products/models.py
+++ src/products/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Product(models.Model):
 	title 			= models.CharField(max_length=120) # max_length is required
 	description 	= models.TextField(blank=True, null=True)
-	price 			= models.DecimalField(max_digits=10, decimal_places=2)
-	summary 		= models.TextField(blank=True, null=False)
+	price 			= models.DecimalField(max_digits=10000, decimal_places=2)
+	summary 		= models.TextField(blank=False, null=False)
 	featured		= models.BooleanField()
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view
+from products.views import product_detail_view
 
 urlpatterns = [
 	path('', home_view, name='home'),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('about/', about_view, name='about'),
 	path('contact/', contact_view, name='contact'),
     path('admin/', admin.site.urls),
+    path('product/', product_detail_view, name='product'),
 ]
